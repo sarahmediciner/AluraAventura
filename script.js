6ca8ba85ae8c0fc4543c848b6298c03d4a84ed06
sarahmediciner <sarahsantana120@hotmail.com>
2024-09-29 15:13:22 -0300

    adicionando assinatura no script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const avanca = document.querySelectorAll('.btn-proximo');
-
+//Sarah Oliveira Santana 3D 29/09/2024
 avanca.forEach(button => {
     button.addEventListener('click', function(){
         const atual = document.querySelector('.ativo');
